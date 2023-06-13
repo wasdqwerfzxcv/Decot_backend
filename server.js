93dbe08d04ac2716d1466b3e220db30888de6fe7
Chiaoyun29 <chiaoyun29@yahoo.com>
2023-06-14 03:26:36 +0800

    Chat backend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./router/authRouter');
+const messageRoutes = require('./router/messageRouter');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,12 +11,24 @@ const corsOptions = {
   optionsSuccessStatus: 200 
 }
 
-
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.use('/auth',authRoutes);
+Message.sync();
 
-app.listen(PORT, async () => {
+app.use('/auth',authRoutes);
+app.use('/', messageRoutes )
+
+ server = app.listen(PORT, function(){
   console.log(`Server is running on port ${PORT}`);
+});
+
+//for chat
+const io = socket(server); //put app.listen in bracket
+io.on('connection', (socket) =>{
+  console.log(socket.id);
+
+  socket.on('SEND_MESSAGE', function(data){
+    io.emit('RECEIVE_MESSAGE', data);
+  });
 });
