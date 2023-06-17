2d7e10846c2610c3133f1cb0702738be05dc4ff8
Chiaoyun29 <chiaoyun29@yahoo.com>
2023-06-17 21:53:21 +0800

    jiayou

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,54 +1,71 @@
-const Message = require('../models/ChatMessage');
+const { Message } = require('../models');
 
-exports.createMessage = async(req, res) => {
-    try {
-        const { author, message, timestamp } = req.body;
-        const newMessage = await Message.create({ author, message, timestamp });
-        res.status(201).json(newMessage);
-    } catch (error) {
-        console.error('Error creating message: ', error);
-        res.status(500).json({ error: 'Error occured while creating the message' });
-    }
-};
-
-exports.getAllMessages = async (req, res) => {
-    try {
-        const messages = await Message.findAll();
-        res.json(messages);
-    } catch (error) {
-        console.error('Error retrieving messages: ', error);
-        res.status(500).json({ error: 'Error occured while retrieving the messages' });
-    }
-};
-
-exports.updateMessage = async (req, res) => {
-    try {
-        const { id } = req.params;
+const createMessage = async(req, res)=>{
+    try{
         const { message } = req.body;
-        const messageToUpdate = await Message.findByPk(id);
-        if (!messageToUpdate) {
-            return res.status(404).json({ error: 'Message not found' });
-        }
-        messageToUpdate.message = message;
-        await messageToUpdate.save();
-        res.json(messageToUpdate);
-    } catch (error) {
-        console.error('Error updating message: ', error);
-        res.status(500).json({ error: 'Error occured while updating the message' });
+        const userId = req.user.id;
+        const msg = await Message.create({
+            message,
+            userId,
+            timestamp: new Date(),
+        });
+        console.log(msg)
+        res.status(201).json({msg});
+    }catch(error){
+        console.error('Error creating message: ', error);
+        res.status(500).json({ error: error.message});
     }
 };
 
-exports.deleteMessage = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const messageToDelete = await Message.findByPk(id);
-        if (!messageToDelete) {
-            return res.status(404).json({ error: 'Message not found' });
+const getAllMessages = async (req,res)=>{
+    try{
+        const userId = req.user.id;
+        console.log('hihi',userId)
+        const messages = await Message.findAll({
+            where: { userId },
+            order: [['createdAt', 'DESC']],
+        });
+        if(messages&&messages.length>0){
+            res.json({ messages });
+        }else{
+            res.json({ messages: [], display: "No messages found" });
         }
-        await messageToDelete.destroy();
-        res.status(204).end();
-    } catch (error) {
-        console.error('Error deleting message: ', error);
-        res.status(500).json({ error: 'Error occured while deleting the message' });
+    }catch(error){
+        console.error('Error retrieving messages: ', error);
+        res.status(500).json({ error: error.message });
     }
+};
+
+// const updateMessage = async (req,res)=>{
+//     try{
+//         const { id }=req.params;
+//         const { message }=req.body;
+//         const messageToUpdate=await Message.findByPk(id);
+//         if(!messageToUpdate){
+//             return res.status(404).json({ error: 'Message not found'});
+//         }
+//         messageToUpdate.message = message;
+//         await messageToUpdate.save();
+//         res.json(messageToUpdate);
+//     }catch(error){
+//         console.error('Error updating message: ', error);
+//         res.status(500).json({ error: 'Error occured while updating the message' });
+//     }
+// };
+
+const deleteMessage = async (req,res)=>{
+    try{
+        const messageId = req.params.messageId;
+        await Message.destroy({ where: { id: messageId }});
+        res.json({ message: 'Message deleted successfully' });
+    }catch(error){
+        console.error('Error deleting message: ', error);
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    createMessage,
+    getAllMessages,
+    deleteMessage
 };
