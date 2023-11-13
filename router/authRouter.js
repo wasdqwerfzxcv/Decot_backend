90293100f4a881005bd7cfb653d62281b97e76d3
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2023-11-14 03:07:24 +0800

    add manage profile

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const { registerUser, loginUser, googleLogin, googleCallback, updateUserRole } = require('../controllers/authController');
+const { registerUser, loginUser, googleLogin, googleCallback, updateUserRole, updateProfile, changePassword, deleteAccount, verifyEnteredPassword, uploadProfilePic} = require('../controllers/authController');
 const { validateRegistration, validateLogin } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.post('/register', validateRegistration, registerUser);
 router.post('/login', validateLogin, loginUser);
 router.get('/google', googleLogin);
 router.get('/google/callback', googleCallback);
 router.put('/updateRole', updateUserRole);
+router.put('/updateProfile', updateProfile);
+router.put('/changePassword', changePassword);
+router.delete('/deleteAccount', deleteAccount);
+router.post('/verifyEnteredPassword', verifyEnteredPassword);
+router.post('/uploadProfilePic/:userId', upload.single('profilePic'), uploadProfilePic);
 
 module.exports = router;
