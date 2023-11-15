8976ef0deb86b5a2d0d279f97c0f22c5464116e5
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2023-11-15 15:55:40 +0800

    add required migration file & remove unnecessary

---
+++ migrations/20211115075127-create-messages.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users', // name of the Users table
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      workspaceId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Workspaces', // name of the Workspaces table
+          key: 'id'
+        },
+      },
+      timestamp: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Messages');
+  }
+};
+
